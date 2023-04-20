9f1614c40f7d9a373f6b2ad6b957c9b38c3008d7
Nikolay_Cholakov <nikolaicholakov25@gmail.com>
2023-04-20 18:17:44 +0300

    Project set up hello world

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles/index.module.css";
+import "./styles/landing.css";
+
+function App() {
+  return <div className="h1">Hello World</div>;
+}
+
+export default App;
